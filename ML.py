2cd18ac2ab3b5c7bb20fbe060011fd73924619e1
DoneKan <102988943+DoneKan@users.noreply.github.com>
2023-07-26 14:58:26 +0300

    Add files via upload

---
+++ ML.py
@@ -0,0 +1,7 @@
+#1st- import the Data
+#2nd - Clean the data (in the Ideal word we have all columns and rows well defined)
+#3rd- split the data . Training |Set/Test set "(100 rows in excel then we decide 80 rows for training some for testing)"
+#4th-  Create a Model (Import an alogorithm, use a library)
+#5th-Check the output 
+#6th-Improve (extra inputts, or change alogorithm)
+
